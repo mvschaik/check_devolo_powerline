a56ad7f8ebc8f12c0467497ff8976009f4b3592d
Maarten van Schaik <mvschaik@gmail.com>
2020-08-13 12:48:32 +0200

    Make pep8 happy

--- check_devolo_powerline.py
+++ check_devolo_powerline.py
@@ -64,12 +64,13 @@ class LegacyDevolo(nagiosplugin.Resource):
         for did, mac, rx, tx in zip(ids, macs, rx_bps, tx_bps):
             if did == '0':
                 continue
+            role = 'DOMAIN_MASTER' if mac == domain_master else 'END_POINT'
             nodes.append({
                 'did': did,
                 'mac': mac,
                 'rx': rx,
                 'tx': tx,
-                'role': 'DOMAIN_MASTER' if mac == domain_master else 'END_POINT',
+                'role': role,
                 })
 
         return nodes
@@ -85,29 +86,38 @@ class Devolo(nagiosplugin.Resource):
 
     def probe(self):
         sysinfo = self.system_info()
-        yield nagiosplugin.Metric('uptime', sysinfo['uptime'], uom='s', min=0, context='uptime')
+        yield nagiosplugin.Metric(
+                'uptime', sysinfo['uptime'], uom='s', min=0, context='uptime')
         for n, load in zip([1, 5, 15], sysinfo['load']):
-            yield nagiosplugin.Metric('load%i' % n, load / 65535, min=0, context='load')
+            yield nagiosplugin.Metric(
+                    'load%i' % n, load / 65535, min=0, context='load')
 
-        yield nagiosplugin.Metric('mem', sysinfo['memory']['total'] -
-                sysinfo['memory']['free'], uom='B', min=0,
-                max=sysinfo['memory']['total'], context='memory')
+        yield nagiosplugin.Metric(
+                'mem', sysinfo['memory']['total'] - sysinfo['memory']['free'],
+                uom='B', min=0, max=sysinfo['memory']['total'],
+                context='memory')
 
         ghninfo = self.ghninfo()
         for device in ghninfo['devices']:
             if device['did'] == ghninfo['device_id']:
                 continue
 
-            if self._remote_mac is None or self._remote_mac.to_lower() == device['mac'].to_lower():
-                yield nagiosplugin.Metric("rx", int(device['rx']), uom='Mbps', min=0, context='dlan')
-                yield nagiosplugin.Metric("tx", int(device['tx']), uom='Mbps', min=0, context='dlan')
+            if self._remote_mac is None or \
+                    self._remote_mac.to_lower() == device['mac'].to_lower():
+                yield nagiosplugin.Metric(
+                        "rx", int(device['rx']), uom='Mbps', min=0,
+                        context='dlan')
+                yield nagiosplugin.Metric(
+                        "tx", int(device['tx']), uom='Mbps', min=0,
+                        context='dlan')
 
-        connected_clients = [client
+        connected_clients = [
+                client
                 for device in self.devices()
                 if device.startswith('ath')
                 for client in self.clients(device)]
-        yield nagiosplugin.Metric('devices', len(connected_clients), min=0,
-                context='devices')
+        yield nagiosplugin.Metric(
+                'devices', len(connected_clients), min=0, context='devices')
 
     def call(self, group, method, **kwargs):
         _log.info("Calling %s.%s(%s)" % (group, method, kwargs))
@@ -128,8 +138,9 @@ class Devolo(nagiosplugin.Resource):
         if self._session_id is None:
             _log.debug("No session ID, logging in")
             self._session_id = "00000000000000000000000000000000"
-            login_info = self.call("session", "login",
-                    username=self._username, password=self._password, timeout=900)
+            login_info = self.call(
+                    "session", "login", username=self._username,
+                    password=self._password, timeout=900)
             self._session_id = login_info["ubus_rpc_session"]
             _log.debug("Got session ID %s" % self._session_id)
         return self._session_id
@@ -173,7 +184,7 @@ class Devolo(nagiosplugin.Resource):
                      'country': 'CH',
                      'disabled': '0',
                      'cfg_disabled': '0',
-                     'supported_rates': ['54000', '48000', '36000', '24000', '18000', '12000', '11000', '9000', '6000', '5500', '2000', '1000'],
+                     'supported_rates': ['54000', '48000', ..., '1000'],
                      'basic_rate': ['11000', '5500', '2000', '1000']}},
              'wifi1':
                 {'frequency': 5600,
@@ -192,44 +203,53 @@ class Devolo(nagiosplugin.Resource):
 
     def clients(self, device):
         """
-            {'clients': 
-                {'E0:AC:CB:9C:62:5A': 
-                    {'connected_time': 156, 
-                     'vendor': {'name': 'Apple', 'description': 'Apple, Inc.'}, 
-                     'rx': {'rate': 5000}, 
-                     'tx': {'rate': 2000}, 
-                     'ipaddr': '192.168.1.22'}, 
-                 'F8:D0:27:D1:3B:0E': 
-                    {'connected_time': 523919, 
-                     'vendor': {'name': 'SeikoEps', 'description': 'Seiko Epson Corporation'}, 
-                     'rx': {'rate': 72000}, 
-                     'tx': {'rate': 72000}, 
+            {'clients':
+                {'E0:AC:CB:9C:62:5A':
+                    {'connected_time': 156,
+                     'vendor': {'name': 'Apple', 'description': 'Apple, Inc.'},
+                     'rx': {'rate': 5000},
+                     'tx': {'rate': 2000},
+                     'ipaddr': '192.168.1.22'},
+                 'F8:D0:27:D1:3B:0E':
+                    {'connected_time': 523919,
+                     'vendor':
+                        {'name': 'SeikoEps',
+                         'description': 'Seiko Epson Corporation'},
+                     'rx': {'rate': 72000},
+                     'tx': {'rate': 72000},
                      'ipaddr': '192.168.1.29'}}}
         """
         return self.call("network.info", "clients", device=device)['clients']
 
     def system_info(self):
         """
-            {'uptime': 522558, 
-             'localtime': 1597311514, 
-             'load': [29024, 24576, 19744], 
-             'memory': {'total': 253546496, 'free': 83570688, 'shared': 278528, 'buffered': 13393920}, 
+            {'uptime': 522558,
+             'localtime': 1597311514,
+             'load': [29024, 24576, 19744],
+             'memory':
+                {'total': 253546496,
+                 'free': 83570688,
+                 'shared': 278528,
+                 'buffered': 13393920},
              'swap': {'total': 0, 'free': 0}}
         """
         return self.call("system", "info")
 
 
-
 @nagiosplugin.guarded
 def main():
     argp = argparse.ArgumentParser()
-    argp.add_argument('-H', '--host', required=True, help='IP address of powerline adapter to check')
-    argp.add_argument('-r', '--remote_mac', help='MAC address of remote DLAN adapter')
+    argp.add_argument('-H', '--host', required=True,
+                      help='IP address of powerline adapter to check')
+    argp.add_argument('-r', '--remote_mac',
+                      help='MAC address of remote DLAN adapter')
     argp.add_argument('-l', '--legacy', action='store_true', default=False)
     argp.add_argument('-v', '--verbose', action='count', default=0)
     args = argp.parse_args()
-    resource = LegacyDevolo(args.host, args.remote_mac) if args.legacy else Devolo(args.host, args.remote_mac)
-    check = nagiosplugin.Check(resource,
+    resource_cls = LegacyDevolo if args.legacy else Devolo
+    resource = resource_cls(args.host, args.remote_mac)
+    check = nagiosplugin.Check(
+            resource,
             nagiosplugin.ScalarContext('dlan'),
             nagiosplugin.ScalarContext('uptime'),
             nagiosplugin.ScalarContext('load'),
@@ -240,6 +260,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
